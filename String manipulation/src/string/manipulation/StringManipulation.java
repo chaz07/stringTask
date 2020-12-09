30cc19b9f36ea8c3ee077fa89e3c098108f7177d
chaz07 <73285136+chaz07@users.noreply.github.com>
2020-12-09 16:27:59 +0000

    Add files via upload

---
+++ String manipulation/src/string/manipulation/StringManipulation.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package string.manipulation;
+
+/**
+ *
+ * @author m2001995
+ */
+public class StringManipulation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+public static void charAt(){
+	String firstName = "John";
+	String surname  = "Wayne";
+	char initial = firstName.charAt(0);
+
+System.out.println("Hello, Mr " + initial + " "  + surname);
+} //end of method
+
+
+}
